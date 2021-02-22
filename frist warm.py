d66767d2cf6ce2bae70c76e3bf6037e665cea9a6
Fmlatti <77768708+Fmlatti@users.noreply.github.com>
2021-02-22 20:13:10 +0800

    Add files via upload

---
+++ frist warm.py
@@ -0,0 +1,48 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+
+
+def get_page_content(url):
+    url = 'http://www.12365auto.com/zlts/0-0-0-0-0-0_0-0-1.shtml'
+    headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64)'
+                             ' AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36'}
+    html = requests.get(url, headers=headers, timeout=10)
+    content = html.text
+    soup = BeautifulSoup(content, 'html.parser')
+    return soup
+
+
+def analysis(soup):
+    df = pd.DataFrame(columns=['id', 'brand', 'car_model', 'type', 'desc', 'problem', 'datetime', 'status'])
+    temp = soup.find('div', class_='tslb_b')
+    tr_list = temp.find_all('tr')
+    for tr in tr_list:
+        td_list = tr.find_all('td')
+        if len(td_list) > 0:
+            id, brand, car_model, type, desc, problem, datetime, status = td_list[0].text, td_list[1].text, td_list[2].text, \
+                                                                          td_list[3].text, td_list[4].text,\
+                                                                          td_list[5].text, td_list[6].text, td_list[7].text
+            # print(id, brand, car_model, type, desc, problem, datetime, status)
+            dic = {}
+            dic['id'] = id
+            dic['brand'] = brand
+            dic['car_model'] = car_model
+            dic['type'] = type
+            dic['desc'] = desc
+            dic['problem'] = problem
+            dic['datetime'] = datetime
+            dic['status'] = status
+            df = df.append(dic, ignore_index=True)
+    return df
+
+
+result = pd.DataFrame(columns=['id', 'brand', 'car_model', 'type', 'desc', 'problem', 'datetime', 'status'])
+url_nonpage = 'http://www.12365auto.com/zlts/0-0-0-0-0-0_0-0-'
+for i in range(20):
+    url = url_nonpage + str(i+1) +'shtml'
+    soup = get_page_content(url)
+    df = analysis(soup)
+    result = result.append(df)
+print(result)
+result.to_excel('car_complain.xlsx', index=False)
